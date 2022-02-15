6e8cf5002230a48b350bdfbf363ed76a65142a8b
Juan David <juandavid-1527@hotmail.com>
2022-02-15 01:37:55 -0500

    Documentation is performed in all classes.

--- Exercise4/src/com/company/Main.java
+++ Exercise4/src/com/company/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Se importa el paquete com.company y las librerias java.text.SimpleDateFormat,
+ * java.util.ArrayList, java.util.Date y java.util.Scanner.
+ *
+ */
 package com.company;
 
 import java.text.SimpleDateFormat;
@@ -5,30 +10,45 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
+/**
+ * Representa la clase principal "main".
+ * @version 1.0.0 2022-02-15
+ * @author Juan David Rojas
+ * @since 1.0.0
+ */
 public class Main {
 
     public static void main(String[] args) {
 
+        /**
+         * Representa el ArrayList de vehiculo.
+         */
         ArrayList<Vehicle> vehicles = new ArrayList<>();
 
+        /**
+         * Representa los atributos a usar.
+         */
         String typeVehicle = null, scrolling = null, dateRegistration;
         int nPassengers = 0, nWheels = 0;
         boolean tripulation = false;
 
         Scanner read = new Scanner(System.in);
 
+        /**
+         * Reepresenta el bluce for que se repite 10 veces.
+         */
         for(int i = 0; i <= 9; i++) {
-            System.out.println("A continuacion ingrese los datos del vehiculo");
+            System.out.println("A continuación ingrese los datos del vehiculo");
             System.out.println("Tipo de vehiculo: ");
             typeVehicle = read.next();
 
-            System.out.println("Numero de pasajeros: ");
+            System.out.println("Número de pasajeros: ");
             nPassengers = read.nextInt();
 
             System.out.println("¿Tiene tripulacion?. Escriba True o false: ");
             tripulation = read.nextBoolean();
 
-            System.out.println("Numero de ruedas: ");
+            System.out.println("Número de ruedas: ");
             nWheels = read.nextInt();
 
             System.out.println("Introduzca la fecha con formato DD/MM/YYYY");
@@ -37,7 +57,9 @@ public class Main {
             SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
             Date testDate = null;
             String date = dateRegistration;
-
+        /**
+         * Representa el try catch para la conversión de un String a Date.
+         */
             try{
                 testDate = df.parse(date);
                 System.out.println("Se convirtió a tipo fecha, " + testDate);
@@ -52,8 +74,7 @@ public class Main {
             System.out.println("Medio por el que se desplaza: ");
             scrolling = read.next();
 
-            Vehicle vehicle = new Vehicle(typeVehicle, nPassengers, tripulation, nWheels, testDate, scrolling) {
-            };
+            Vehicle vehicle = new Vehicle(typeVehicle, nPassengers, tripulation, nWheels, testDate, scrolling);
             vehicles.add(vehicle);
         }
         for(Vehicle vehicle: vehicles) {
--- Exercise4/src/com/company/Vehicle.java
+++ Exercise4/src/com/company/Vehicle.java
@@ -1,9 +1,22 @@
+/**
+ * Se importa el paquete com.company y las librerias
+ * java.text.SimpleDateFormat y java.util.Date
+ */
 package com.company;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Representa la clase Vehicle.
+ * @version 1.0.0 2022-02-15
+ * @author Juan David Rojas.
+ * @since 1.0.0
+ */
 public class Vehicle {
+    /**
+     * Representa los atributos de la clase Vehicle
+     */
     private String typeVehicle;
     private int nPassengers;
     private boolean tripulation;
@@ -11,6 +24,15 @@ public class Vehicle {
     private Date dateRegistration;
     private String scrolling;
 
+    /**
+     * Representa el constructor de la clase Vehicle.
+     * @param typeVehicle de tipo String.
+     * @param nPassengers de tipo int.
+     * @param tripulation de tipo boolean.
+     * @param nWheels de tipo int.
+     * @param dateRegistration de tipo Date
+     * @param scrolling de tipo String.
+     */
     public Vehicle(String typeVehicle, int nPassengers, boolean tripulation, int nWheels, Date dateRegistration, String scrolling) {
         this.typeVehicle = typeVehicle;
         this.nPassengers = nPassengers;
@@ -24,58 +46,111 @@ public class Vehicle {
 
     }
 
+    /**
+     * Representa el método getTypeVehicle.
+     * @return el tipo de vehiculo.
+     */
     public String getTypeVehicle() {
         return typeVehicle;
     }
 
+    /**
+     * Representa el método setTypeVehicle.
+     * @param typeVehicle de tipo String.
+     */
     public void setTypeVehicle(String typeVehicle) {
         this.typeVehicle = typeVehicle;
     }
 
+    /**
+     * Representa el método getnPassengers.
+     * @return el numero de pasajeros.
+     */
     public int getnPassengers() {
         return nPassengers;
     }
 
+    /**
+     * Representa el método setnPassengers.
+     * @param nPassengers de tipo int.
+     */
     public void setnPassengers(int nPassengers) {
         this.nPassengers = nPassengers;
     }
 
+    /**
+     * Representa el método isTripulation.
+     * @return un boolean si hay tripulación o no.
+     */
     public boolean isTripulation() {
         return tripulation;
     }
 
+    /**
+     * Representa el método setTripulation.
+     * @param tripulation de tipo boolean.
+     */
     public void setTripulation(boolean tripulation) {
         this.tripulation = tripulation;
     }
 
+    /**
+     * Representa el método getnWheels.
+     * @return el número de ruedas.
+     */
     public int getnWheels() {
         return nWheels;
     }
 
+    /**
+     * Representa el método setnWheels.
+     * @param nWheels de tipo int.
+     */
     public void setnWheels(int nWheels) {
         this.nWheels = nWheels;
     }
 
+    /**
+     * Representa el método getDateRegistration.
+     * @return la fecha de la matricula.
+     */
     public Date getDateRegistration() {
         return dateRegistration;
     }
 
+    /**
+     * Representa el método setDateRegistration.
+     * @param dateRegistration de tipo Date.
+     */
     public void setDateRegistration(Date dateRegistration) {
         this.dateRegistration = dateRegistration;
     }
-
+    /**
+     * Representa el método getScrolling.
+     * @return el medio de desplazamiento.
+     */
     public String getScrolling() {
         return scrolling;
     }
 
+    /**
+     * Representa el método setScrolling.
+     * @param scrolling de tipo String.
+     */
     public void setScrolling(String scrolling) {
         this.scrolling = scrolling;
     }
 
+    /**
+     * Representa el método de aceleración.
+     */
     public void Accelerate() {
         System.out.println("Acelerar");
     };
 
+    /**
+     * Representa el método de parar.
+     */
     public void Stop() {
         System.out.println("Parar");
     };
@@ -86,9 +161,13 @@ public class Vehicle {
         return dateFormat;
     }
 
+    /**
+     * Representa una herencia de la clase Vehicle.
+     * @return los atributos de la clase Vehicle.
+     */
     @Override
     public String toString() {
-        return "Vehicle{" +
+        return "Vehiculo{" +
                 "Tipo de vehiculo='" + getTypeVehicle() + '\'' +
                 ", Número de pasajeros=" + getnPassengers() +
                 ", Tripulación=" + isTripulation() +
