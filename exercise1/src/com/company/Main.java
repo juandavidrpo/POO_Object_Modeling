8e375d8abc31e39257f5453147ba2be7dd80a045
Juan David <juandavid-1527@hotmail.com>
2022-02-15 00:17:39 -0500

    Some functionalities of the main class are modified and the documentation is done

--- exercise1/src/com/company/Main.java
+++ exercise1/src/com/company/Main.java
@@ -1,15 +1,30 @@
+/**
+ * Se importa el paquete com.company
+ * y las librerias JOptionPane y ArrayList.
+ */
 package com.company;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
+
+/**
+ * Reprenseta la clase principal "Main"
+ * @version 1.0.0 2022-02-14
+ * @author Juan David Rojas.
+ * @since 1.0.0
+ */
 public class Main {
 
     public static void main(String[] args) {
-
+/**
+ * Representa los atributos a usar.
+ */
         String selection = null;
-        double gravity = 6.673E-11;
-        int limit = 0;
-
+        final double gravity = 6.673E-11;
+        int end = 0;
+/**
+ * Representa un ArrayList del sistema solar, con datos especificos de los planetas y el sol.
+ */
         ArrayList<PlanetarySystem> system = new ArrayList<>();
         PlanetarySystem sun = new PlanetarySystem(1.989 * 10E30, 1.41, 1390000, 0, 1, "Sol" + "\n");
         PlanetarySystem p1 = new PlanetarySystem(3.285 * 10E23, 5.43, 4879, 5.79*10E10, 2, "Mercurio" + "\n");
@@ -30,13 +45,24 @@ public class Main {
         system.add(p7);
         system.add(p8);
 
-        while (limit == 0) {
-
-            int selectionMenu = JOptionPane.showOptionDialog(null, "Calcular atraccion gravitatoria", "Menu",
+        /**
+         * Representa un bucle while que no deja finalizar el programa hasta que el usuario decida hacerlo.
+         */
+        while (end == 0) {
+        /**
+         * Representa el menú principal con las opciones a calcular.
+         */
+            int selectionMenu = JOptionPane.showOptionDialog(null, "Calcular atracción gravitatoria", "Menú",
                     JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-                    new Object[]{"De dos cuerpos", "Con respecto al sol", "Cerrar"}, "opcion 1");
-
+                    new Object[]{"Entre dos cuerpos", "De un cuerpo con respecto al sol", "Cerrar"}, "opcion 1");
+        /**
+         * Representa la sentencia switch para navegar entre las opciones del menú.
+         */
             switch (selectionMenu + 1) {
+                /**
+                 * El caso 1 representa la opcion de elegir entre dos cuerpos del sistema solar
+                 * para despues hacer el calculo gravitacional.
+                 */
                 case 1:
                 String[] options = new String[system.size()];
 
@@ -47,17 +73,44 @@ public class Main {
                 String planet1 = (String) JOptionPane.showInputDialog(null, "Selecciona un planeta y presiona aceptar",
                         selection, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
+        /**
+         * Representa el índice de la selección del planeta que se haga en el primer menú desplegable.
+         */
                     int indexPlanet1 = Integer.parseInt(planet1.split(". ")[0]) - 1;
 
                 String planet2 = (String) JOptionPane.showInputDialog(null, "Selecciona otro planeta y presiona aceptar",
                         selection, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
+                    if(planet1 == planet2){
+                        throw new IllegalArgumentException("No se puede calcular el mismo cuerpo");
+                    }
+
+        /**
+         * Representa el índice de la selección del planeta que se haga en el segundo menú desplegable.
+         */
                     int indexPlanet2 = Integer.parseInt(planet2.split(". ")[0]) - 1;
-                    double squaredDistance = Math.pow((system.get(indexPlanet2).getDistanceSun()), 2);
+
+        /**
+         * Representa el cálculo de restar la distancia de dos cuerpos con respecto al sol
+         * para identificar la distancia de ambos cuerpos.
+         */
+                    double distancePlanet = Math.abs(system.get(indexPlanet1).getDistanceSun() - system.get(indexPlanet2).getDistanceSun());
+        /**
+         * Representa el cálculo para elevar la distancia al cuadrado.
+         */
+                    double squaredDistance = Math.pow(distancePlanet, 2);
+
+        /**
+         * Representa la formula de la atracción gravitatoria para calcular la atracción de dos cuerpos.
+         */
                     double attraction = gravity * ((system.get(indexPlanet1).getMass() * (system.get(indexPlanet2).getMass()) / squaredDistance));
-                    System.out.println("La atraccion gravitatoria de ambos cuerpos es de: " + attraction + " N");
+                    System.out.println("La atracción gravitatoria de ambos cuerpos es de: " + attraction + " N");
                 break;
 
+
+        /**
+         * El caso 2 representa la opción directa para calcular un cuerpo y el sol.
+         */
                 case 2:
                     String[] option = new String[system.size()];
 
@@ -65,19 +118,27 @@ public class Main {
                         option[i] = system.get(i).getIdentifier() + ". " + system.get(i).getName();
                     }
 
-                    double sunMass = 1.989 * 10E30;
-
                     String planet = (String) JOptionPane.showInputDialog(null, "Selecciona un planeta y presiona aceptar",
                             selection, JOptionPane.QUESTION_MESSAGE, null, option, option[1]);
-
+        /**
+         * Representa el índice del cuerpo seleccionado.
+         */
                     int indexPlanet = Integer.parseInt(planet.split(". ")[0]) - 1;
-                    double squaredDistances = Math.pow((system.get(indexPlanet).getDistanceSun()), 2);
-                    double attractions = gravity * ((system.get(indexPlanet).getMass() * sunMass) / squaredDistances);
-                    System.out.println("La atraccion gravitatoria de ambos cuerpos es de: " + attractions + " N");
 
-                case 3:
+        /**
+         * Representa el calculo para elevar al cuadrado la distancia del cuerpo seleccionado.
+         */
+                    double squaredDistances = Math.pow((system.get(indexPlanet).getDistanceSun()), 2);
+
+        /**
+         * Representa la formula de la atracción gravitatoria para calcular la atracción de un cuerpo
+         * con respecto al sol.
+         */
+                    double attractions = gravity * ((system.get(indexPlanet).getMass() * sun.getMass()) / squaredDistances);
+                    System.out.println("La atracción gravitatoria de ambos cuerpos es de: " + attractions + " N");
+                    break;
                 default:
-                limit = 1;
+                end = 1;
             }
         }
     }
--- exercise1/src/com/company/PlanetarySystem.java
+++ exercise1/src/com/company/PlanetarySystem.java
@@ -1,6 +1,18 @@
+/**
+ * Se importa el paquete com.company
+ */
 package com.company;
 
+/**
+ * Representa la clase PlanetarySystem.
+ * @version 1.0.0 2022-02-14
+ * @author Juan David Rojas.
+ * @since 1.0.0
+ */
 public class PlanetarySystem {
+    /**
+     * Representa los atributos de la clase PlanetarySystem.
+     */
     private double mass;
     private double density;
     private int diameter;
@@ -8,6 +20,15 @@ public class PlanetarySystem {
     private int identifier;
     private String name;
 
+    /**
+     * Representa el constructor de la clase PlanetarySystem.
+     * @param mass de tipo double.
+     * @param density de tipo double.
+     * @param diameter de tipo int.
+     * @param distanceSun de tipo double.
+     * @param identifier de tipo int.
+     * @param name de tipo String.
+     */
     public PlanetarySystem(double mass, double density, int diameter, double distanceSun, int identifier, String name) {
         this.mass = mass;
         this.density = density;
@@ -17,50 +38,99 @@ public class PlanetarySystem {
         this.name = name;
     }
 
+    /**
+     * Representa el metodo getMass.
+     * @return la masa de un cuerpo.
+     */
+
     public double getMass() {
         return mass;
     }
 
+    /**
+     * Representa el metodo setMass.
+     * @param mass de tipo double.
+     */
     public void setMass(double mass) {
         this.mass = mass;
     }
 
+    /**
+     * Representa el metodo getDensity.
+     * @return la densidad de un cuerpo.
+     */
     public double getDensity() {
         return density;
     }
 
+    /**
+     * Representa el metodo setDensity.
+     * @param density de tipo double.
+     */
     public void setDensity(double density) {
         this.density = density;
     }
 
+    /**
+     * Representa el metodo getDiameter.
+     * @return el diametro de un cuerpo.
+     */
     public int getDiameter() {
         return diameter;
     }
 
+    /**
+     * Representa el metodo setDiameter.
+     * @param diameter de tipo int.
+     */
     public void setDiameter(int diameter) {
         this.diameter = diameter;
     }
 
+    /**
+     * Representa el metodo getDistanceSun.
+     * @return la distancia al sol.
+     */
     public double getDistanceSun() {
         return distanceSun;
     }
 
+    /**
+     * Representa el metodo setDistanceSun.
+     * @param distanceSun de tipo double.
+     */
     public void setDistanceSun(double distanceSun) {
         this.distanceSun = distanceSun;
     }
 
+    /**
+     * Representa el metodo getIdentifier.
+     * @return el identificador de un cuerpo.
+     */
     public int getIdentifier() {
         return identifier;
     }
 
+    /**
+     * Representa el metodo setIdentifier.
+     * @param identifier de tipo int.
+     */
     public void setIdentifier(int identifier) {
         this.identifier = identifier;
     }
 
+    /**
+     * Representa el metodo getName.
+     * @return el nombre de un cuerpo.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Representa el metodo setName.
+     * @param name de tipo String.
+     */
     public void setName(String name) {
         this.name = name;
     }
